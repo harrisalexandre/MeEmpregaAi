81d4c6c7ed9b90e4c967c209876e64b211d8b2ec
HarrisAlexandre <harrisalexandre308@gmail.com>
2021-10-12 00:42:15 -0300

    configuracao cors.and

--- src/main/java/com/compasso/meempregaai/config/security/SecurityConfigurations.java
+++ src/main/java/com/compasso/meempregaai/config/security/SecurityConfigurations.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @Configuration
